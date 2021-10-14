8e3507f73d92fdc35a6d0c5d434761a4882b8860
Jaehun Park <pjaehun4122@gmail.com>
2021-10-14 13:35:29 -0700

    trying to add my project on github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
+import Amiibo from "./amiibo";
+import styled from "styled-components";
 
-function App() {
+const App =()=> {
+  useEffect(()=>{
+    getAmiibo();
+  },[])
+  const [amiibos, setAmiibos]= useState([]);
+  const [search, setSearch]=useState('');
+  
+
+
+  const getAmiibo= async()=>{
+    const response = await fetch('https://amiiboapi.com/api/amiibo/');
+    const data = await response.json();
+    
+    console.log(data)
+
+
+    setAmiibos(data.amiibo.slice(0,50));
+
+  }
+  const figures=amiibos.filter(function(amiibo){
+    return amiibo.type==="Figure";
+  });
+
+
+const updateSearch= event=>{
+  setSearch(event.target.value)
+}
+  const StyledApp=styled.div`
+  display:flex;
+  flex-wrap:wrap;
+  justify-content:center;
+  margin:1% 1%;
+  `
+  const StyledNav=styled.nav`
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  `
+
+  const AtagDiv=styled.div`
+  width:50%;
+  display:flex;
+  justify-content:space-evenly;
+ 
+  align-items:center;
+  `
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StyledNav>
+        
+        <img src={process.env.PUBLIC_URL + '/logo-amiibo-glow.png'}></img>
+  <form className="search-form">
+      <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
+      <button  className="search-button" type="submit">reset</button>
+     </form>
+
+        <AtagDiv>
+        <a>what is amiibo?</a>
+        <a>Lineup</a>
+        <a>FAQ</a>
+        <a>Buy now</a>
+        </AtagDiv>
+      </StyledNav>
+
+   
+
+      <StyledApp>
+     {figures.filter((val)=>{
+       if (setSearch===""){
+         return val
+       }else if(val.character.trim().toLowerCase().includes(search.toLowerCase())){
+         return val
+       }
+     }).map(amiibo=>(
+        <Amiibo key={amiibo.tail} character={amiibo.character} image={amiibo.image} game={amiibo.gameSeries}/>
+      ))}
+     </StyledApp>
+
+
+      
     </div>
   );
 }
---
+++ src/amiibo.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled from 'styled-components';
+
+ const Amiibo=(props)=>{
+     const {character,image,game}=props;
+    
+
+    const StyledAmiibo=styled.div`
+    display:flex;   
+    border: 0 solid #14213d;
+    width:25%;
+    padding:1% 1%;
+    margin:1%;
+    background-color:#e5e5e5;
+    color:black;
+    &:hover{
+        color:#3c6e71;
+    }
+
+    `
+    const StyledImg=styled.img`
+    max-width: 20vh;
+    max-height: 20vh;
+    object-fit:cover;
+    margin:auto;
+    transition: transform .4s;
+
+    &:hover{
+        transform: scale(1.2);
+    }
+    `
+
+
+
+    return(
+        <StyledAmiibo>
+            <StyledImg src={image}></StyledImg>
+            <div>
+                <h1>{character}</h1>
+                <p>from {game}</p>
+            </div>
+
+        </StyledAmiibo>
+    );
+}
+
+export default Amiibo;
